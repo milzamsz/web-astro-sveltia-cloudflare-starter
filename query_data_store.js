3ca52853b3f99695060c94be3d81f9623731968a
milzamsz <milzamsz@gmail.com>
2026-06-26 11:12:59 +0700

    fix: resolve ESLint errors in source code

--- query_data_store.js
+++ query_data_store.js
@@ -6,9 +6,7 @@ try {
   // devalue.parse is used for standard devalue serialization.
   // Let's see if we can parse it.
   const data = parse(content);
-  console.log('Keys in data store:', Array.from(data.keys()));
   const pages = data.get('pages');
-  console.log('Pages entries count:', pages.size);
   for (const [key, val] of pages.entries()) {
     console.log(`Page Key: ${key}`);
     console.log(`  locale: ${val.data.locale}`);
